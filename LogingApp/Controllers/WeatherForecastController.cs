c59866a63cb4482c940db833aa17530a70db64e8
Mithu0229 <showkat131080@gmail.com>
2024-02-10 07:57:51 +0600

    Add project files.

---
+++ LogingApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogingApp.Controllers
+{
+    //https://datalust.co/download and install and run play button in web
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Made call to Weather Endpoint");
+            try
+            {
+                throw new Exception("THis is our logging test exception");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+            .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Fatal Error Occurred");
+                throw;
+            }
+            
+        }
+    }
+}
